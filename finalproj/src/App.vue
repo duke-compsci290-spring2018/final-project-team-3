b42c83c5d8b7390ead8bb4049729d2e8d5726050
TAYLOR ALBUS <taylor.albus@duke.edu>
2018-04-18 19:30:20 -0400

    made some image edits

--- finalproj/src/App.vue
+++ finalproj/src/App.vue
@@ -1,34 +1,34 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+ 	<div id="app">
+
+		<img v-if="picOne" @mouseover="picOne = false" src="./assets/camhead.png">
+		<img v-if="!picOne" @mouseleave="picOne = true" src="./assets/camhead2.png">
+		<h1>{{ msg }}</h1>
+	</div>
 </template>
+
+
+
+
+
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+	name: 'app',
+ 	data () {
+		return {
+			msg: "King's Cuts",
+			picOne: true
+    	}
+  	}
+	
+	
 }
 </script>
+
+
+
+
 
 <style lang="scss">
 #app {
