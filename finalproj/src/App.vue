80e56e288991923b84c3205038ac8c9e50c257f6
TAYLOR ALBUS <taylor.albus@duke.edu>
2018-04-19 02:00:58 -0400

    made a lot of changes to site, mostly interface stuff

--- finalproj/src/App.vue
+++ finalproj/src/App.vue
@@ -1,16 +1,38 @@
 <template>
  	<div id="app">
-		<div v-if="picOne">
-			<h1>From</h1>
-			<img @mouseover="picOne = false" src="./assets/camhead.png">
-			<h1>scrawn</h1>
+		
+		<h1 class="siteName" @click="loggingIn = false">{{ msg }}</h1>
+		
+		<h4 @click="loggingIn = true" v-if="!loggingIn">LOG IN</h4>
+		
+		<header v-if="!loggingIn">
+			
+			<a class="feed" @click="testing">Feed</a>
+			<a class="find" @click="testing">Find</a>
+			<a class="share" @click="testing">Share</a>
+			<a class="settings" @click="testing">Settings</a>
+			
+		</header>
+		
+		<login v-if="loggingIn">
+		</login>
+
+		
+<!--		Picture changes when hovered over-->
+		<div class="camPic" v-if="!loggingIn">
+			<div v-if="picOne">
+				<h2>From</h2>
+				<img @mouseover="picOne = false" src="./assets/camhead.png">
+				<h2>scrawn</h2>
+			</div>
+			<div v-if="!picOne">
+				<h2>to</h2>
+				<img @mouseleave="picOne = true" src="./assets/camhead2.png">
+				<h2>jawn</h2>
+			</div>
 		</div>
-		<div v-if="!picOne">
-			<h1>to</h1>
-			<img @mouseleave="picOne = true" src="./assets/camhead2.png">
-			<h1>jawn</h1>
-		</div>
-		<h1>{{ msg }}</h1>
+	
+		
 	</div>
 </template>
 
@@ -20,14 +42,26 @@
 
 
 <script>
+	
+import Login from './components/Login.vue'
+	
 export default {
 	name: 'app',
  	data () {
 		return {
 			msg: "King's Cuts",
-			picOne: true
+			picOne: true,
+			loggingIn: false
     	}
-  	}
+  	},
+	methods: {
+		testing: function () {
+			console.log("clicked it!")
+		}
+	},
+	components: {
+		Login
+	}
 	
 	
 }
@@ -38,30 +72,112 @@ export default {
 
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+	  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	  -webkit-font-smoothing: antialiased;
+	  -moz-osx-font-smoothing: grayscale;
+	  text-align: center;
+	  color: #2c3e50;
+	  margin-top: 0;
+	}
+	
+	body {
+		margin-left: 0;
+		margin-right: 0;
+	}
+	
+	h1, h2 {
+	  font-weight: normal;
+	}
+	
+	h1 {
+/*		font-size: 70px;*/
+		font-size: 6vw;
+		margin-bottom: 0;
+		margin-top: 0;
+		cursor: pointer;
+	}
 
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+	ul {
+	  	list-style-type: none;
+	  	padding: 0;
+	}
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+	a {
+	  	display: inline-block;
+	  	margin: 0 0px;
+		text-align: center;
+/*		font-size: 3vw;*/
+		font-size: 20px;
+		padding-right: 5%;
+		padding-left:  5%;
+		padding-top: 2vh;
+		background-color: #c2d1ed;
+		padding-bottom: 2vh;
+		cursor: pointer;
+	}
+	
+	h4 {
+		float: right;
+		position: fixed;
+		top: 20px;
+		right: 30px;
+		cursor: pointer;
+	}
+	
+	.leftLinks {
+		float: left;
+		align-content: center;
+	}
+	
+	header {
+		width: 100%;
+		text-align: center;
+/*		padding-top: 2vh;*/
+/*		padding-bottom: 2vh;*/
+		background-color: #c2d1ed;	
+	}
+	
+	.siteName {
+		height: 90%;
+	}
+	
+	.camPic {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+		width: 50%;
+	}
 
-a {
-  color: #42b983;
-}
+	.feed:hover {
+		background-color: #a4bce8;
+	}
+	
+	.find:hover {
+		background-color: #a4bce8;
+	}
+	
+	.share:hover {
+		background-color: #a4bce8;
+	}
+	
+	.settings:hover {
+		background-color: #a4bce8;
+	}
+	
+	#href {
+		color: "red";
+	}
+	
+	a:link {
+		color: #2c3e50;
+		text-decoration: none;
+	}
+	
+	a:visited {
+		color: #2c3e50;
+	}
+	
+	
 </style>
---
+++ finalproj/src/components/login.vue
@@ -0,0 +1,91 @@
+<template>
+	<div id="login">
+		<br>
+		
+		<div v-if="!creating && !loggingIn">
+			<h3 @click="creating = true" href="#">Create account</h3>
+
+			<h3 @click="loggingIn = true" href="#">Log in</h3>
+		</div>
+		
+		<form v-if="creating">
+			<button v-on:click="creating=false">Go back</button>
+			<br><br>
+			Your email: 
+			<br><br>
+			<input v-model="email" placeholder="e.g. john.doe@duke.edu">
+			<br>
+			<br>
+			<button v-on:click="createAcc">Create account</button>
+<!--			<h5 v-if="invalid">Enter a valid email</h5>-->
+			<h5>{{ validated }}</h5>
+		</form>
+		
+		<form v-if="loggingIn">
+			<button v-on:click="loggingIn=false">Go back</button>
+			<br><br>
+			Your email: 
+			<br><br>
+			<input v-model="email" placeholder="e.g. john.doe@duke.edu">
+			<br>
+			<br>
+			<button v-on:click="logUserIn">Log in</button>
+			
+		
+		</form>
+	
+	</div>
+
+
+
+
+</template>
+
+<script>
+
+export default {
+	
+	name: 'login',
+	data () {
+		return {
+			creating: false,
+			loggingIn: false,
+			email: '',
+			invalid: false,
+			validated: ''
+		}
+	},
+	methods: {
+		createAcc: function() {
+			if (this.email != '' && this.email.indexOf("@") > -1 && this.email.indexOf(".") > -1) {
+				console.log("valid email")
+				this.validated = "Valid email, you've been added to the system"
+			} else {
+				this.validated = "Please enter a valid email"
+			}
+			this.email = ''
+		},
+		logUserIn: function() {
+			
+		}
+	}
+}
+	
+	
+
+
+</script>
+
+<style>
+
+	input {
+		width: 20%;
+		text-align: center;
+	}
+	
+	h3 {
+		cursor: pointer;
+	}
+	
+
+</style>
