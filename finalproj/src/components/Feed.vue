f56cd6d5157b7ede44c213dbbba4843e12073c56
Taylor Albus <taylor.albus@duke.edu>
2018-04-30 00:33:34 -0400

    i hate you camer

--- finalproj/src/components/Feed.vue
+++ finalproj/src/components/Feed.vue
@@ -11,37 +11,18 @@
 			
 			<p> Filter by Cost:
 			<label for="one">$</label>
-<<<<<<< HEAD
-      		<input type="checkbox" id="$" value="One" v-model="costSort">
-=======
 
       		<input type="checkbox" id="$" value="$" v-model="costSort">
->>>>>>> ac64b7d799ebc84cb37825f7a19e80beb130268e
 			<label for="two">$$</label>
-			<input type="checkbox" id="$$" value="Two" v-model="costSort">
+			<input type="checkbox" id="$$" value="$$" v-model="costSort">
 			<label for="two">$$$</label>
-<<<<<<< HEAD
-			<input type="checkbox" id="$$$" value="Three" v-model="costSort">
-=======
 			<input type="checkbox" id="$$$" value="$$$" v-model="costSort">
 
 
->>>>>>> ac64b7d799ebc84cb37825f7a19e80beb130268e
 			<br>
-			<span>Picked: {{ costSort }}</span>
+			<span>Picked: {{ picked }}</span>
 			
 			<p> Filter by Rating:
-<<<<<<< HEAD
-			<input type="radio" id="one" value="5 Star" v-model="rateSort">
-			<label for="one">5 Star</label>
-			<input type="radio" id="two" value="4 Star" v-model="rateSort">
-			<label for="two">4 Star</label>
-			<input type="radio" id="two" value="3 Star" v-model="rateSort">
-			<label for="two">3 Star</label>
-			<input type="radio" id="two" value="2 Star" v-model="rateSort">
-			<label for="two">2 Star</label>
-			<input type="radio" id="two" value="1 Star" v-model="rateSort">
-=======
 
 			<input type="radio" id="one" value="★ ★ ★ ★ ★" v-model="rateSort">
 			<label for="one">5 Star</label>
@@ -53,17 +34,17 @@
 			<label for="two">2 Star</label>
 			<input type="radio" id="two" value="★ " v-model="rateSort">
 
->>>>>>> ac64b7d799ebc84cb37825f7a19e80beb130268e
 			<label for="two">1 Star</label>
 			<br>
 			<span>Picked: {{ rateSort }}</span>
 			
 			
 			<p> Filter by Hair Type: </p>	
+
 			<select  v-model="selected" multiple>
-				<option>Short</option>
-				<option>Long</option>
-				<option>Medium</option>
+				<option>Short (length)</option>
+				<option>Long (length)</option>
+				<option>Medium (length)</option>
 				<option>Curly</option>
 				<option>Wavy</option>
 				<option>Straight</option>
@@ -77,8 +58,9 @@
 				<option>Bearded</option>
 				<option>Bun</option>
 				<option>Pixie</option>
-				<option>Part</option>
+				<option>Part (middle, side, etc)</option>
 				</select>
+
 				<br><br>
 			<span> Selected: {{ selected }}</span>
 			
@@ -89,9 +71,7 @@
 		<!-- Feed-->
 		<div class="outerDiv">
 <!--		<div v-for="image in reversedUsers" class="eachDiv" :alreadyLiked="alreadyLiked">-->
-<!--			<div class="eachDiv" v-for="image in filteredUsers">-->
 			<div class="eachDiv" v-for="image in filteredUsers">
-				
 				<h3>Stylist: {{ image.reviewStylist }}</h3>
 				<h3 @click="setLocation(image)" class="locText">Location: <strong>{{ image.reviewShop }}</strong></h3>  
 				<h3>User: {{ image.reviewer }}</h3>
@@ -133,8 +113,8 @@ export default {
 			hey: "howdy",
 			city: "", 
 			selected: [], 
-			costSort: [], 
-			rateSort:[]
+			picked: [], 
+			picked1:[]
 		}
 	},
 	
@@ -170,11 +150,6 @@ export default {
             return this.users.reverse();
 		},
 		
-		combinedList() {
-			var newArray = this.costSort.concat(this.rateSort);
-			return newArray
-		},
-		
 		filteredUsers: function () {
 			var self = this
 			return self.reversedUsers.filter(function (user) {
@@ -192,43 +167,18 @@ export default {
 				}
 			})
 		}
-<<<<<<< HEAD
-}
-//	
-//		filteredPeople: function() {
-//			var vm = this;
-//			var category = vm.selectedCategory;
-//			var gender = vm.selectedGender;
-//
-//			if(category === "All" && gender === "All") {
-//				//save performance, juste return the default array:
-//				return vm.people;
-//			} else {
-//				return vm.people.filter(function(person) {
-//					//return the array after passimng it through the filter function:
-//					return  (category === 'All' || person.category === category) && (gender === 'All'  || person.gender === gender);	 
-//
-//				});
-//			}
-//		}
-//	}
-//});
-//	
-	
-	
-=======
 
 
 
 	}
 }
 
->>>>>>> ac64b7d799ebc84cb37825f7a19e80beb130268e
 	
 
 
 </script>
 <style scoped>
+
 	h1 {
 		font-size: 36px;
 		background-color: #bee8e7;
@@ -256,6 +206,8 @@ export default {
 		padding-top: 15px;
 /*		text-align: center;*/
 /*		margin-right: 5px;*/
+
+
 	}
 	
 	.eachText {
@@ -290,6 +242,7 @@ export default {
 	}
 	
 	.theReview {
+
 	}
 	
 	.locText {
@@ -307,11 +260,6 @@ export default {
 		padding-bottom: 0;
 		margin-top: 4px;
 		margin-bottom: 4px;
-	}
-	
-	select {
-		width: 10%;
-		height: 80px;
 	}
 	
 	
